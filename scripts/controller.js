ef0ca6edaf44c1b5be1e04e9ecd1dccd5182e148
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-11-04 17:51:48 -0800

    getUser works for navbar

--- scripts/controller.js
+++ scripts/controller.js
@@ -7,35 +7,32 @@ var app = angular.module('stamplay', ['ngStamplay'])
 //CONTROLLER
 app.controller('appController', ["$scope", "$stamplay", "appFactory", function($scope, $stamplay, appFactory){
 
-	$scope.signUp = function(email, password){
-		
-		var registrationData = {
-			email: email,
-			password: password
-			};
 
-		var newUser = new Stamplay.User().Model;
-		newUser.signup(registrationData).then(function(){
-			window.location = "home.html";
-		});
-	};
 
-	$scope.login = function(email,password){
-
-		var user = new Stamplay.User().Model;
-		user.login(email, password).then(function(){
-  			window.location = "home.html";
-		});
-	};
+  /****************************/
+  /*  GET USER & USERs TASK   */
+  /****************************/
+  appFactory.getUser().then(function(user) {
+  	var email = user.get("email");
+  	$scope.email = email;
+  });
 
 }]);
 
 
 //FACTORY
 app.factory('appFactory', ["$q", function($q) {
-  	return {
-    	test : function(){
-      		console.log('hit factory');
-    	}
-	};
+  return {
+    getUser : function(){
+      var q = $q.defer();
+      var user = new Stamplay.User().Model;
+      user.currentUser().then(function(){
+        q.resolve(user);
+      });
+      return q.promise;
+    }
+
+
+
+  };
 }]);
--- scripts/factory.js
+++ scripts/factory.js
@@ -1,15 +1,52 @@
 
-// var app = angular.module('stamplay', ['ngStamplay'])
-// .run(function() {
-//  	Stamplay.init("trelloclone");
-// });
+// //CONTROLLER
+// app.controller('appController', ["$scope", "$stamplay", "appFactory", function($scope, $stamplay, appFactory){
 
+// //REGISTER NEW USER//
+// 	$scope.signUp = function(email, password){
+// 		var registrationData = {
+// 			email: email,
+// 			password: password
+// 			};
+
+// 		var user = new Stamplay.User().Model;
+// 		user.signup(registrationData).then(function(){
+// 			window.location = "home.html";
+// 		});
+// 	};
+
+// //LOGIN USER//
+// 	$scope.login = function(email,password){
+// 		var user = new Stamplay.User().Model;
+// 		user.login(email, password).then(function(){
+//   			window.location = "home.html";
+// 		});
+// 	};
+
+// }]);
+
+// //LOAD ALL DATA WHEN PAGE LOADS//
+// // 	var user = new Stamplay.User().Model;
+// // 	user.currentUser().then(function(user){
+// // 		console.log(user.email);
+// // 		$scope.name = user;
+// // });
+	
+// 	appFactory.getUser().then(function(user){
+// 		console.log(user);
+// 	});
 
 // //FACTORY
 // app.factory('appFactory', ["$q", function($q) {
 //   	return {
-//     	test : function(){
-//       		console.log('hit factory');
+//     	getUser : function(){
+//       		var q = $q.defer();
+//       		var user = new Stamplay.User().Model;
+//       		user.currentUser().then(function(){
+//         		q.resolve(user);
+//       		});
+//       		return q.promise;
 //     	}
 // 	};
+
 // }]);
