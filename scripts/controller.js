9335cdcb117db9ab42ad98d103d7e1e809471305
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-11-04 18:32:25 -0800

    basic layout for navbar complete

--- scripts/controller.js
+++ scripts/controller.js
@@ -5,13 +5,9 @@ var app = angular.module('stamplay', ['ngStamplay'])
 });
 
 //CONTROLLER
-app.controller('appController', ["$scope", "$stamplay", "appFactory", function($scope, $stamplay, appFactory){
+app.controller('homeController', ["$scope", "$stamplay", "appFactory", function($scope, $stamplay, appFactory){
 
-
-
-  /****************************/
-  /*  GET USER & USERs TASK   */
-  /****************************/
+ //GET USER INFO ON PAGE LOAD
   appFactory.getUser().then(function(user) {
   	var email = user.get("email");
   	$scope.email = email;
@@ -19,8 +15,34 @@ app.controller('appController', ["$scope", "$stamplay", "appFactory", function($
 
 }]);
 
+//CONTROLLER
+app.controller('userController', ["$scope", "$stamplay", "appFactory", function($scope, $stamplay, appFactory){
 
-//FACTORY
+//REGISTER NEW USER
+	$scope.signUp = function(email, password){
+		var registrationData = {
+			email: email,
+			password: password
+			};
+
+		var user = new Stamplay.User().Model;
+		user.signup(registrationData).then(function(){
+			window.location = "home.html";
+		});
+	};
+
+//LOGIN USER//
+	$scope.login = function(email,password){
+		var user = new Stamplay.User().Model;
+		user.login(email, password).then(function(){
+  			window.location = "home.html";
+		});
+	};
+
+}]);
+
+
+//FACTORY----------------------------------------------------------------------------//
 app.factory('appFactory', ["$q", function($q) {
   return {
     getUser : function(){
